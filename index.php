680b5313f3aa5c370a1c944b2cb4a868af7b4c37
trgthntn <85030355+trgthntn@users.noreply.github.com>
2021-07-01 10:48:44 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- header -->
+<?php include("header.php"); ?>
+<!-- End of header -->
+
+<body>
+
+<!-- nav bar -->
+<?php include("nav.php"); ?>
+<!-- End of nav bar -->
+
+<!-- Index -->
+<div class="container">
+    <div class="row py-3">
+        <div class="col-12 text-primary py-3">
+            <h4>Thế giới</h4>                
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <h5>Đà Nẵng trình Thủ tướng duyệt 3 khu công nghiệp 15.800 tỷ đồng</h5>
+            <p>3 khu công nghiệp mới gồm Hòa Cầm (giai đoạn 2), Hòa Nhơn và Hòa Ninh vừa được thành phố Đà Nẵng trình Thủ tướng phê duyệt.</p>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <h5>Chủ tịch Ủy ban chứng khoán: 'Chúng tôi nợ nhà đầu tư lời xin lỗi'</h5>
+            <p>Việc nghẽn lệnh, theo Chủ tịch Ủy ban chứng khoán Trần Văn Dũng, không chỉ vì thị trường tăng quá nóng mà có một phần trách nhiệm của cơ quan điều hành.</p>
+        </div> 
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <h5>Doanh nghiệp muốn lùi thu phí hạ tầng cảng biển thêm một năm</h5>
+            <p>TP HCM Cho rằng đang kiệt quệ vì dịch, doanh nghiệp muốn việc thu phí hạ tầng cảng biển dời thêm một năm thay vì 3 tháng như đề xuất của thành phố.</p>
+        </div> 
+    </div>
+</div>
+<!-- End of Index -->
+
+<!-- footer -->
+<?php include("footer.php"); ?>
+<!-- End of footer -->
+
+</body>
+</html>
